6c946e90fe553ccfbced8e584a446b35e8ce51b6
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-15 00:36:54 -0400

    Finished the CRUD functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,19 +4,4 @@ class Post < ApplicationRecord
                       
     validates :body, presence: true,
                      length: { maximum: 500 }
-
-    validate :title_length_must_be_more_than_5
-    validate :title_must_start_with_r
-
-    def title_length_must_be_more_than_5
-        if title.length < 6
-            errors.add(:title, "can't be less than 5")
-        end
-    end
-
-    def title_must_start_with_r
-        if title[0] != 'R'
-            errors.add("The first letter must be letter R")
-        end
-    end
 end
