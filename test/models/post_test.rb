529d7ac0b5b08979c57dbac8fbad03dcf2f4a9d5
Rommel Torquator <rommeltorquator09@gmail.com>
2021-06-11 11:30:43 -0400

    Finished the MVP

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,11 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save post without title' do
+    post = Post.new
+    post.body = "This is the test body hohohohohoho merry christmas"
+
+    assert_not post.save, 'saved post without title' # must return false
+  end
 end
